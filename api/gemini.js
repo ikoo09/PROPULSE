78ad1c9b8aef2deb2aea66f0e7c4f6af90415bd6
ikoo09 <nicodamara0909@gmail.com>
2026-06-26 18:37:56 +0700

    Add files via upload

--- api/gemini.js
+++ api/gemini.js
@@ -30,7 +30,7 @@ module.exports = async function handler(req, res) {
   try {
     // Solusi Error 404 Model Not Found: 
     // Menggunakan alias "-latest" agar Google API dapat menemukan modelnya
-    const modelName = "gemini-1.5-flash-latest"; 
+    const modelName = "Gemini 2.5 Flash"; 
     
     // Jika masih gagal juga, Anda bisa mencoba mengubah modelName di atas menjadi:
     // const modelName = "gemini-pro";
